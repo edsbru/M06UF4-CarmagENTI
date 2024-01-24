5875a3785307b5fef6ae038f0bf2edae27c6f9c3
edsbru <eduardo.salazar@enti.cat>
2024-01-24 20:51:43 +0100

    coches dos pantallas + balas

--- index.js
+++ index.js
@@ -1,53 +1,70 @@
+//Variables que contienen los modulos que realizan esas acciones
 let static = require('node-static');
 let http = require('http');
 let ws = require('ws');
 
-var file = new static.Server('./public');
- 
+//Instancia de static que accede a la carpeta public
+let file = new static.Server('./public');
+
+//Variable que envia los archivos de la carpeta 'public' a la web
+//'end' es un evento que espera a que se termine de enviar los datos
 let http_server = http.createServer(function (request, response) {
     request.addListener('end', function () {
         file.serve(request, response);
     }).resume();
 });
 
+//Instancia el servidor de WS
 let ws_server = new ws.WebSocketServer({ server: http_server });
 
+//Escuchamos el puerto de web 8080
 http_server.listen(8080);
 
+//Creamos variables para guardar las conexiones que hagan los jugadores
 let p1_conn;
 let p2_conn;
 
+//Cuando se haga una conexión mandamos 'EVENT Connection' a la conso
 ws_server.on('connection', function (conn) {
 
-  console.log('EVENT: Connection');
+	console.log('EVENT: Connection');
 
-  if(p1_conn == undefined){
-    p1_conn = conn;
-
-    p1_conn.send('{"player_num":1}');
-    
-	p1_conn.on('message', function(data){
-      if (p2_conn == undefined)
-	    return;
-	
-	  p2_conn.send(data.toString());
-
-	  console.log(data.toString());
-   });
+	//Si no hay nadie conectado como j1
+	if(p1_conn == undefined){
+		//se le asigna p1_conn
+		p1_conn = conn;
+		//enviamos que jugador eres
+    	p1_conn.send('{"player_num":1}');
+    	//Cuando ekl p1 envie datos
+		p1_conn.on('message', function(data){
+			//si no hay p2, no hace nada
+			if (p2_conn == undefined)
+	    		return;
+			
+			//si hay p2 se le envian los datos en String
+			p2_conn.send(data.toString());
+			console.log(data.toString());
+		});
   
-  }
-  else if (p2_conn == undefined){
-    p2_conn = conn;
-
-    p2_conn.send('{"player_num":2}');
-   
-    p2_conn.on('message', function(data){
-	  if (p1_conn == undefined)
-	    return;
-
-      p1_conn.send(data.toString());
-    });
-  }
+	}
+	
+	//Si no hay nadie conectado como p2
+	else if (p2_conn == undefined){
+		//se le asigna p2_conn
+    	p2_conn = conn;
+		//enviamos que jugador eres
+		p2_conn.send('{"player_num":2}');
+   		//cuando el p2 envie datos
+    	p2_conn.on('message', function(data){
+		
+			//si no hay p1, no hace nada
+			if (p1_conn == undefined)
+	    		return;
+			
+			//si hay p1, se le envian datos
+			p1_conn.send(data.toString());
+    	});
+  	}
 /*
   conn.on('message', function(data){
     console.log(data.toString());
--- public/carmagenti.js
+++ public/carmagenti.js
@@ -2,8 +2,11 @@
 
 //#TODO
 //Que es / hace function(event)?
+//Función anónima, se usa para pasar funciones como parámetros
 //Que es / hace socket?
+//Es lo que conecta el cliente con el servidor, es el "tubo"
 //Que es / hace data?
+//Es una prpiedad de event, cuya información nos ha llegado a través de los eventos 'open' o 'message'
 
 //Empezamos siendo jug 0, esto está arriba para que lo vea todo el script
 let player_num = 0;
@@ -12,42 +15,43 @@ let player_num = 0;
 let player1;
 let player2;
 
-//#TODO Creamos una variable donde guardar la ip del servidor o del socket...?
+// Creamos una variable donde guardar la ip del servidor al que vamos a hacer conexión
 const socket = new WebSocket('ws://10.40.1.132:8080');
 
-//#TODO Abrimos un listener que espera a...X y hace function(event)?
+//Abrimos un listener que espera a que se abra la conexión con un cliente y mete los datos en function(event)
 socket.addEventListener('open', function(event){
 
 });
 
-//#TODO Mandamos un mensaje diciendo "Server", por que?
+//Mandamos un mensaje diciendo "Server", para comprobar que nos llegan datos
 socket.addEventListener('message', function(event){
 	console.log('Server: ', event.data);
 
-	//#TODO Hacemos una variable data que parsea...que?
+	//Hacemos una variable data que parsea a JavaScript el texto recivido por 'message'
 	let data = JSON.parse(event.data);
 
-	//#TODO Si data.player_num ¿no es undefined?
+	//Si data.player_num tiene valor (diferente de undefined) significa que nos envia nuestro numero de jugador.
 	if(data.player_num != undefined){
-  		//#TODO Guardamos en una variable "data.player_num"?
+  		//Guardamos el numero del jugador que viene de los datos que hemos recibido
 		player_num = data.player_num;
 		console.log('Jugador num:', player_num);
 	}
 
-	//#TODO Si >data< NO es undefined
+	// Si recibimos coordenadas
 	else if (data.x != undefined){
 	
-	//Si el valor de player_num es 2
-	if(player_num == 2){
-		//ponemos los datos del coche p1 en la variable "player1"
-		player1.x = data.x;
-		player1.y = data.y;
-		player1.rotation = data.r;
+	//Si el valor de player_num es 2 (somos el jugador 2)
+		if(player_num == 2){
+			//recibimos los datos del coche del  p1 en  "player1"
+			player1.x = data.x;
+			player1.y = data.y;
+			player1.rotation = data.r;
 		}
 	}
 });
 
 //Cosas de phaser
+//Configuración de Phaser
 const config = {
 	type: Phaser.AUTO,
 	width: 800,
@@ -58,7 +62,7 @@ const config = {
 		update: update
 	}
 }
-
+//Inicialización de Phaser
 const game = new Phaser.Game(config);
 
 //Variables con Velocidad y velocidad de rotación de los coches
@@ -68,29 +72,42 @@ const CAR_ROTATION = 2;
 //Var que guarda el angulo del coche
 let player1_angle = 0; 
 
-//#TODO Variable que...?
+//Variable que guarda el control por flechas(que se llaman cursores)
 let cursors;
+
+//#TODO Var que guarda el control de la spacebar
+let spacebar;
 
 function preload ()
 {
-	//cargamos las imágenes de los coches
+	//cargamos las imágenes
+	this.load.image('background', 'assets/racetrack.jpg');
 	this.load.image('car1', 'assets/PNG/Cars/car_red_small_1.png');
 	this.load.image('car2', 'assets/PNG/Cars/car_blue_small_1.png');
+	this.load.image('bullet1', 'assets/PNG/Objects/barrel_red.png');
+	this.load.image('bullet2', 'assets/PNG/Objects/barrel_blue.png');
 }
 
 function create ()
 {
-	//#TODO CREAMOS/INSTANCIAMOS los coches y ¿cursors...?
+	//CREAMOS/INICIALIZAMOS los coches y el objeto que registra los inputs.
+	background = this.add.image(400, 300, 'background');
 	player1 = this.add.image(400, 300, 'car1');
 	player2 = this.add.image(500, 300, 'car2');
+	bullet1 = this.add.image(300, 300, 'bullet1');
+	bullet2 = this.add.image(300, 350, 'bullet2');
 	cursors = this.input.keyboard.createCursorKeys();
+	spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+	bullet1.setVisible(false);
+	bullet2.setVisible(false);
 }
 
 function update ()
 {
-	//Si el player_num es 0
+	//Si el player_num es 0 (No nos hemos conectado)
 	if (player_num == 0){
-		//#TODO ¿No pasa nada...?
+		//No pasa nada
 		return;
 	}
 
@@ -112,15 +129,26 @@ function update ()
 	else if (cursors.right.isDown){
 		player1_angle += CAR_ROTATION;
 	}
+
+	spacebar.on('down', (key, event) => {
+		bullet1.x = player1.x;
+		bullet1.y = player1.y - 60;
+		bullet1.setVisible(true);
+	});
   
-	//#TODO ¿Angle y rotation son diferentes?
+	//rotation es la acción de girar y angle son los grados.
 	player1.rotation = player1_angle*Math.PI/180;
-  
+
 	//Hacemos una variable que guarda las coordenadas del jugador1
-	var player_data = {
+	let player_data = {
 		x: player1.x,
 		y: player1.y,
 		r: player1.rotation
+	};
+
+	let bullet1_data = {
+		bx: bullet1.x,
+		by: bullet1.y
 	};
 
 	//Mandamos por el socket los datos del jugador1 en STRING
