dcb6d3b1fd636f91e74320eb20a8fe0c12c1591b
edsbru <eduardo.salazar@enti.cat>
2024-01-24 17:43:45 +0100

    Comments

--- index.js
+++ index.js
@@ -23,19 +23,29 @@ ws_server.on('connection', function (conn) {
 
   if(p1_conn == undefined){
     p1_conn = conn;
-    p1_conn.send('{"player_num":1}');
-    p1_conn.on('message', function(data){
-     //p2_conn.send(data));
 
-	console.log(data.toString());
+    p1_conn.send('{"player_num":1}');
+    
+	p1_conn.on('message', function(data){
+      if (p2_conn == undefined)
+	    return;
+	
+	  p2_conn.send(data.toString());
+
+	  console.log(data.toString());
    });
   
   }
   else if (p2_conn == undefined){
     p2_conn = conn;
+
     p2_conn.send('{"player_num":2}');
+   
     p2_conn.on('message', function(data){
-    	p1_conn.send(data);
+	  if (p1_conn == undefined)
+	    return;
+
+      p1_conn.send(data.toString());
     });
   }
 /*
--- public/carmagenti.js
+++ public/carmagenti.js
@@ -1,79 +1,129 @@
+//CARMAGENTI.JS
+
+//#TODO
+//Que es / hace function(event)?
+//Que es / hace socket?
+//Que es / hace data?
+
+//Empezamos siendo jug 0, esto está arriba para que lo vea todo el script
 let player_num = 0;
 
+//Declaramos las variables de los jugadores.
+let player1;
+let player2;
+
+//#TODO Creamos una variable donde guardar la ip del servidor o del socket...?
 const socket = new WebSocket('ws://10.40.1.132:8080');
 
+//#TODO Abrimos un listener que espera a...X y hace function(event)?
 socket.addEventListener('open', function(event){
 
 });
 
+//#TODO Mandamos un mensaje diciendo "Server", por que?
 socket.addEventListener('message', function(event){
-  console.log('Server: ', event.data);
-  let data = JSON.parse(event.data);
+	console.log('Server: ', event.data);
 
-  if(data.player_num != undefined){
-   player_num = data.player_num;
-   console.log('Jugador num:', player_num);
-  }
+	//#TODO Hacemos una variable data que parsea...que?
+	let data = JSON.parse(event.data);
+
+	//#TODO Si data.player_num ¿no es undefined?
+	if(data.player_num != undefined){
+  		//#TODO Guardamos en una variable "data.player_num"?
+		player_num = data.player_num;
+		console.log('Jugador num:', player_num);
+	}
+
+	//#TODO Si >data< NO es undefined
+	else if (data.x != undefined){
+	
+	//Si el valor de player_num es 2
+	if(player_num == 2){
+		//ponemos los datos del coche p1 en la variable "player1"
+		player1.x = data.x;
+		player1.y = data.y;
+		player1.rotation = data.r;
+		}
+	}
 });
 
+//Cosas de phaser
 const config = {
- type: Phaser.AUTO,
- width: 800,
- height: 600,
- scene: {
-  preload: preload,
-  create: create,
-  update: update
- }
+	type: Phaser.AUTO,
+	width: 800,
+	height: 600,
+	scene: {
+		preload: preload,
+		create: create,
+		update: update
+	}
 }
 
 const game = new Phaser.Game(config);
 
+//Variables con Velocidad y velocidad de rotación de los coches
 const CAR_SPEED = 2;
 const CAR_ROTATION = 2;
 
+//Var que guarda el angulo del coche
 let player1_angle = 0; 
 
+//#TODO Variable que...?
 let cursors;
-
-let player1;
-
 
 function preload ()
 {
-  this.load.image('car1', 'assets/PNG/Cars/car_red_small_1.png');
-  this.load.image('car2', 'assets/PNG/Cars/car_blue_small_1.png');
+	//cargamos las imágenes de los coches
+	this.load.image('car1', 'assets/PNG/Cars/car_red_small_1.png');
+	this.load.image('car2', 'assets/PNG/Cars/car_blue_small_1.png');
 }
 
 function create ()
 {
-  player1 = this.add.image(400, 300, 'car1');
-  player2 = this.add.image(500, 300, 'car2');
-  cursors = this.input.keyboard.createCursorKeys();
+	//#TODO CREAMOS/INSTANCIAMOS los coches y ¿cursors...?
+	player1 = this.add.image(400, 300, 'car1');
+	player2 = this.add.image(500, 300, 'car2');
+	cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update ()
 {
-  if (cursors.up.isDown){
-   player1.y -= CAR_SPEED*Math.cos(player1_angle*Math.PI/180);
-   player1.x += CAR_SPEED*Math.sin(player1_angle*Math.PI/180);
-  }
+	//Si el player_num es 0
+	if (player_num == 0){
+		//#TODO ¿No pasa nada...?
+		return;
+	}
 
-  if (cursors.left.isDown){
-   player1_angle -= CAR_ROTATION;
-  }
+	//Si player_num es 1
+	if (player_num == 1){
 
-  else if (cursors.right.isDown){
-   player1_angle += CAR_ROTATION;
-  }
-
-  player1.rotation = player1_angle*Math.PI/180;
+	//Si la "flecha arriba" se pulsa, haz mates
+	if (cursors.up.isDown){
+		player1.y -= CAR_SPEED*Math.cos(player1_angle*Math.PI/180);
+		player1.x += CAR_SPEED*Math.sin(player1_angle*Math.PI/180);
+	}
   
-  var player_data = {
-    x: player1.x,
-	y: player1.y,
-	r: player1.rotation
-  };
+	//Si la "flecha izquierda" se pulsa haz mates y gira el coche
+	if (cursors.left.isDown){
+		player1_angle -= CAR_ROTATION;
+  	}
+
+	//Si la "flecha derecha" se pulsa haz mates para girar el coche
+	else if (cursors.right.isDown){
+		player1_angle += CAR_ROTATION;
+	}
   
-  socket.send(JSON.stringify(player_data));
+	//#TODO ¿Angle y rotation son diferentes?
+	player1.rotation = player1_angle*Math.PI/180;
+  
+	//Hacemos una variable que guarda las coordenadas del jugador1
+	var player_data = {
+		x: player1.x,
+		y: player1.y,
+		r: player1.rotation
+	};
+
+	//Mandamos por el socket los datos del jugador1 en STRING
+	socket.send(JSON.stringify(player_data));
+	}
 }
